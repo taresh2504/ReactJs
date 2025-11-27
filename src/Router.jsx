1695c98dc021f22c191d2486fe8f4a68365662bf
taresh2504 <taresh25202@gmail.com>
2025-11-27 19:24:23 +0530

    router

---
+++ src/Router.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Router = () => {
+  return (
+    <>
+    <h1>Router</h1>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import App from './tailwind.jsx'
+import App from './Router.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>  
   // </StrictMode>,
 )

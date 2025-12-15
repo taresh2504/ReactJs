d98bd5554420f40f71c935e48d288d924129c0d8
taresh2504 <taresh25202@gmail.com>
2025-12-15 16:29:15 +0530

    get&post

--- src/Datashow.jsx
+++ src/Datashow.jsx
@@ -10,13 +10,62 @@ export const Datashow = () => {
       console.log(res,data)
       setdata(res.data)
     }).catch((err)=>{
+
       console.log('Error',err);
+
     })
+
+    useEffect(()=>{
+      FetchData()
+    })
+
   },[])
+
+  let DataDelete=(id)=>{
+
+    let api = `http://localhost:3000/movie/${id}`
+    axios.delete(api).then(()=>{
+      alert('Booking cancelled')
+      FetchData()
+    })
+
+  }
   return (
     <>
-    <div>Datashow</div>
-    
+    <h1>Data show Page</h1>
+    <table border={2}>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Aadhar</th>
+          <th>City</th>
+          <th>Checkin</th>
+          <th>Checkout</th>
+          <th>People</th>
+          <th>Total</th>
+          <th>Canacel</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        { data.map((e)=>(
+          
+          <tr>
+            <td>{e.name}</td>
+            <td>{e.age}</td>
+            <td>{e.aadharno}</td>
+            <td>{e.city}</td>
+            <td>{e.checkin}</td>
+            <td>{e.checkout}</td>
+            <td>{e.people}</td>
+            <td>{e.total}</td>
+            <td onClick={()=>{DataDelete(e.id)}}>Delete</td>
+          </tr>
+        ))
+        }
+      </tbody>
+    </table>
     </>
   )
 }
